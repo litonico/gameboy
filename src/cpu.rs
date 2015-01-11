fef49b69194ffc4d88555d895a34593d8fe6fa37
Lito Nicolai <lito.nicolai@gmail.com>
2015-01-11 12:16:14 -0800

    Move arithmatic into an ALU

--- src/cpu.rs
+++ src/cpu.rs
@@ -48,6 +48,7 @@ struct Z80 {
     _clock: Clock,
     _r: RegisterSet,
     MMU: ::mmu::MMU,
+    ALU: ::alu::ALU,
 }
 
 macro_rules! _load(
@@ -79,28 +80,6 @@ macro_rules! _load_nn( // Load
 
         $cpu._clock.m += 3;
         $cpu._clock.t += 12;
-    })
-);
-
-macro_rules! _inc(
-    ($cpu:ident, $reg:ident) => ({
-        $cpu._r.$reg += 1;
-
-        $cpu._r.f |= ($cpu._r.f & CARRY)
-        if $cpu._r.$reg == 0x00 {
-            $cpu._r.f |= ZERO;
-        }
-        if $cpu._r.$reg & 0x0F {
-            $cpu._r.f |= HALF;
-        }
-    })
-);
-
-macro_rules! _add(
-    ($cpu:ident, $reg1:ident, $reg2:ident) => ({
-        $cpu._r.$reg1 = $cpu._r.$reg2;
-        $cpu._clock.m += 1;
-        $cpu._clock.t += 4;
     })
 );
 
